d9b7e61c393c21d5cc413f547971c01abb526c96
Nikita Shevchenko <nik2302vin@gmail.com>
2026-07-17 12:51:09 +0300

    fornmatting

--- src/api/contactsservice.js
+++ src/api/contactsservice.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL:"http://localhost:5000/contacts",
-  headers:{"Content-Type":'application/json'}
-})
+  baseURL: "http://localhost:5000/contacts",
+  headers: { "Content-Type": "application/json" },
+});
